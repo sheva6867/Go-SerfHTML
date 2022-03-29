c06191f2fbd55ab37df2d9a43271325a5e7a867d
sheva6867 <sheva6867@gmail.com>
2022-03-30 00:50:55 +0300

    daythree

--- app/js/main.js
+++ app/js/main.js
@@ -5,13 +5,29 @@ $('.header_slider').slick({
     fade:true,
     prevArrow: ' <img class="slider-arrows slider-arrows__left" src="images/arrows-left.svg" alt=""></img>',
     nextArrow: ' <img class="slider-arrows slider-arrows__right" src="images/arrows-right.svg" alt=""></img>',
-    asNavFor: '.slider-dots',
+    asNavFor: '.slider-dotshead',
 });
 
-$('.slider-dots').slick({
+$('.slider-dotshead').slick({
     slidesToShow: 4,
     slidesToScroll: 4,
     asNavFor: '.header_slider',
 });
   
+$('.serf-slider').slick({
+    slidesToShow: 4,
+    slidesToScroll: 1,
+    prevArrow: ' <img class="slider-arrows slider-arrows__left" src="images/arrows-left.svg" alt=""></img>',
+    nextArrow: ' <img class="slider-arrows slider-arrows__right" src="images/arrows-right.svg" alt=""></img>',
+    asNavFor:'.slider-map',
+});
+
+$('.slider-map').slick({
+    slidesToShow: 8,
+    slidesToScroll: 1,
+    arrows:false,
+    asNavFor: '.serf-slider',
+    focusOnSelect: true,
+})
+
 });
